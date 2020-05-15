9d0e9eae6243ac9c83d1e6f7ffb5e1a9d4804c19
avojarot <64825394+avojarot@users.noreply.github.com>
2020-05-15 10:11:58 +0300

    Course_Project_Kizitskyi

---
+++ Справочник_Гидролога/HydrologistGuideLibrary1/DAL/DAO.cs
@@ -0,0 +1,47 @@
+﻿using Справочник_Гидролога.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Справочник_Гидролога.DAL
+{
+    class DAO
+    {
+        HydrologistGuide guide;
+        string filePath = "guide.bin";
+        public DAO(HydrologistGuide guide)
+        {
+            this.guide = guide;
+        }
+
+        public void Save()
+        {
+            using (Stream stream = File.Create(filePath))
+            {
+                var serializer = new BinaryFormatter();
+                serializer.Serialize(stream, guide);
+            }
+        }
+        public void Load()
+        {
+            using (Stream stream = File.OpenRead(filePath))
+            {
+                var serializer = new BinaryFormatter();
+                HydrologistGuide gu = (HydrologistGuide)serializer.Deserialize(stream);
+                Copy(gu.Articles, guide.Articles);
+                Copy(gu.Tests, guide.Tests);
+                Copy(gu.Users, guide.Users);
+            }
+
+            void Copy<T>(List<T> list1, List<T> list2)
+            {
+                list2.Clear();
+                list2.AddRange(list2);
+            }
+        }
+    }
+}
---
+++ Справочник_Гидролога/HydrologistGuideLibrary1/models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Справочник_Гидролога.models
+{
+    [Serializable]
+    abstract class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
